84dc4bd023de8183a864ebe801b038562607494e
Neha1239 <86868752+Neha1239@users.noreply.github.com>
2022-03-23 14:52:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference=0;
+rightwristX=0;
+leftwristX=0;
 function setup() {
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -8,6 +11,11 @@ function setup() {
 }
 function draw() {
     background('#ff4f4b');
+    document.getElementById("font_size").innerHTML="The size of the sentence will be= "+difference+"px";
+    fill('#000000');
+    stroke('#000000');
+    textSize(difference);
+    text('Neha Talathi',20,400);
     }
     function modelLoaded() {
         console.log('poseNet is initialized');
@@ -15,5 +23,9 @@ function draw() {
     function gotPoses(results) {
         if (results.length>0) {
             console.log(results);
+            leftwristX=results[0].pose.leftWrist.x;
+            rightwristx=results[0].pose.rightWrist.x;
+            difference=floor(leftwristX-rightwristX);
+            console.log("leftwristX= "+leftwristX+" rightwristX= "+rightwristX+" difference= "+difference);
         }
     }
